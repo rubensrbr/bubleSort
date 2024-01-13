ea280821704ec899dd416d65fccbd501438b11aa
Rubens <rubandrumline@gmail.com>
2024-01-12 23:54:38 -0300

    more reusable code

--- build/index.js
+++ build/index.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log("123");
+Object.defineProperty(exports, "__esModule", { value: true });
+const NumbersCollection_1 = require("./NumbersCollection");
+const Sorter_1 = require("./Sorter");
+const numbersCol = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter_1.Sorter(numbersCol);
+sorter.sort();
+console.log(numbersCol.data);
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { NumbersCollection } from "./NumbersCollection";
+import { Sorter } from "./Sorter";
 
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const numbersCol = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCol);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCol.data);
