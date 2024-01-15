5e237a106a74a7be02c373f267e8b24331bd3d4f
Rubens <rubandrumline@gmail.com>
2024-01-14 23:14:07 -0300

    Finishing

--- build/index.js
+++ build/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const NumbersCollection_1 = require("./NumbersCollection");
-const Sorter_1 = require("./Sorter");
-const numbersCol = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter_1.Sorter(numbersCol);
-sorter.sort();
-console.log(numbersCol.data);
+// const numbersCol = new NumbersCollection([10, 3, -5, 0]);
+// numbersCol.sort();
+// console.log(numbersCol.data);
+// const characterCol = new CharactersCollection("Xaayb");
+// characterCol.sort();
+// console.log(characterCol.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
-const numbersCol = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCol);
-sorter.sort();
-console.log(numbersCol.data);
+// const numbersCol = new NumbersCollection([10, 3, -5, 0]);
+// numbersCol.sort();
+// console.log(numbersCol.data);
+
+// const characterCol = new CharactersCollection("Xaayb");
+// characterCol.sort();
+// console.log(characterCol.data);
